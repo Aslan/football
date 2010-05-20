04e9ac639ea3bbada210a75b7429b8fa1977fff5
Aslan <aslan@socialvibe.com>
2010-05-19 21:52:54 -0700

    comments added, what_column plugin added

---
+++ app/models/comment.rb
@@ -0,0 +1,21 @@
+class Comment < ActiveRecord::Base
+
+  # === List of columns ===
+  #   id               : integer 
+  #   user_id          : integer 
+  #   content          : text 
+  #   active           : boolean 
+  #   ancestory        : string 
+  #   commentable_id   : integer 
+  #   commentable_type : string 
+  #   created_at       : datetime 
+  #   updated_at       : datetime 
+  # =======================
+
+	belongs_to :commentable, :polymorphic => true
+	has_ancestry
+	belongs_to :user
+	attr_accessible :user_id
+	
+  attr_accessible :author, :content, :active, :ancestory, :commentable_id, :commentable_type
+end
