873493749c4a5238e643ea810a8dd923615bbf8b
Aslan <aslan@socialvibe.com>
2010-05-25 18:20:09 -0700

    validation is working with ajax and jquery

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,6 +7,7 @@ class PostsController < ApplicationController
   def show
     @post = Post.find(params[:id])
 		@comments = @post.comments
+		@commentable = @post # because i like to have a polymorphic names inside comments views. 
   end
   
   def new
