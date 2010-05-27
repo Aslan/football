9c37e2ef236b31028e28d3e00454009067e16307
Aslan <aslan@socialvibe.com>
2010-05-26 18:26:56 -0700

    jquery work

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,12 +2,19 @@
 // This file is automatically included by javascript_include_tag :defaults
 
 jQuery.ajaxSetup({
-	'beforeSend' : function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
+    'beforeSend' : function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
 })
 $(document).ready(function() {
-	$("#new_comment").submit(function() {
-		$.post($(this).attr("action"), $(this).serialize(), null, "script");
-		return false;
-	})
-	
+    $("#new_comment").submit(function() {
+        $.post($(this).attr("action"), $(this).serialize(), null, "script");
+        return false;
+    })
 })
+
+// $(document).ready(function(){
+//     $("#comments a").click(function(event){
+//          $(this).parent().hide("slow");
+//          event.preventDefault();
+//    });
+// });
+    
